8edc06e16792abe0b57415c86ea25f0377e9afbb
Rydial <kimdanny2001@gmail.com>
2022-04-17 16:51:55 +1000

    Added a Server Config file and Added Firefox HTTPS Support

--- run.py
+++ run.py
@@ -29,15 +29,15 @@ import controller
 
 #-----------------------------------------------------------------------------
 
-# It might be a good idea to move the following settings to a config file and then load them
-# Change this to your IP address or 0.0.0.0 when actually hosting
-host = 'localhost'
+# Read Server Configurations from Server Config File
+config = {}
 
-# Test port, change to the appropriate port to host
-port = 8081
+with open("server.config") as file:
+    for line in file:
+        param, value = line.strip().split("=")
+        config[param] = value
 
-# Turn this off for production
-debug = True
+#-----------------------------------------------------------------------------
 
 def run_server():    
     '''
@@ -45,13 +45,13 @@ def run_server():
         Runs a bottle server
     '''
     run(
-        host=host,
-        port=port,
-        debug=debug,
-        server="gunicorn",
-        reloader=1,
-        keyfile="certs/info2222.test..key",
-        certfile="certs/info2222.test..crt")
+        host=config["host"],
+        port=int(config["port"]),
+        server=config["server"],
+        keyfile=config["keyfile"],
+        certfile=config["certfile"],
+        reloader=int(config["reloader"]),
+        debug=bool(config["debug"]))
 
 #-----------------------------------------------------------------------------
 # Optional SQL support
